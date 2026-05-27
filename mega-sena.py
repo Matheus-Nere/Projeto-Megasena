ea4500350bd7116e8e7b0c7ec4298231edcbb6a0
Matheus-Nere <nerematheus50@gmail.com>
2026-05-27 20:04:41 -0300

    primeiro commit

---
+++ mega-sena.py
@@ -0,0 +1,31 @@
+import random
+print("--------------------------------------------------------------------")
+print()
+print("                          MEGASENA                                  ")
+print()
+print("--------------------------------------------------------------------")
+print()
+print()
+qtdjogos = int(input("Quantos jogos você deseja fazer? : "))
+print()
+
+
+def gerador (jogos):
+    objeto = {}
+    for i in range(jogos):
+        lista = []
+        jogo = 0
+        while (jogo < 6):
+            numero_aleatorio = random.randint(1, 60)
+            lista.append(numero_aleatorio)
+            jogo += 1
+        if (len(lista) == 6) :   
+            objeto[i +1] = lista
+            lista = []
+    return objeto
+ 
+sorteios = gerador(qtdjogos)
+print("os jogos sorteados foram:")
+for i in sorteios:
+    print(sorteios[i])
+print()
